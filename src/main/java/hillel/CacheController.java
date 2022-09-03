09ef8fd57931c44652b498a5dc32caa016910640
Константин Довбыш <dokongr@gmail.com>
2022-09-03 10:00:14 +0300

    Все методы вебконтроллера реализованы

--- src/main/java/hillel/CacheController.java
+++ src/main/java/hillel/CacheController.java
@@ -7,15 +7,16 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class CacheController {
     @Autowired
-    private ApplicationContext applicationContext;
+    private CacheService cacheService;
 
     //  http://localhost:8080/create?name=appcache
     @GetMapping("/create")
-    boolean create(@RequestParam(name = "name") String cacheName){
-        CacheService cacheService = applicationContext.getBean(CacheService.class);
+    boolean create(@RequestParam("name") String cacheName){
         cacheService.create(cacheName);
         return cacheService.isCacheExist(cacheName);
     }
@@ -23,15 +24,36 @@ public class CacheController {
     //  http://localhost:8080/isexist?name=appcache
     @GetMapping("/isexist")
     boolean isCacheExist(@RequestParam("name") String cacheName) {
-        CacheService cacheService = applicationContext.getBean(CacheService.class);
         return cacheService.isCacheExist(cacheName);
     }
 
     //  http://localhost:8080/put?name=appcache&key=data&value=1234567890
     @GetMapping("/put")
-    boolean put(@RequestParam(name = "name") String cacheName, @RequestParam String key, @RequestParam String value) {
-        CacheService cacheService = applicationContext.getBean(CacheService.class);
+    boolean put(@RequestParam("name") String cacheName, @RequestParam String key, @RequestParam String value) {
         return cacheService.put(cacheName, key, value);
     }
+
+    //  http://localhost:8080/get?name=appcache&key=data
+    @GetMapping("/get")
+    String get(@RequestParam("name") String cacheName, @RequestParam String key){
+        return Optional.ofNullable(cacheService.get(cacheName, key)).orElse("null");
+    }
+
+    //  http://localhost:8080/clear
+    @GetMapping("/clearall")
+    String clear(){
+        cacheService.clear();
+        return "ok";
+    }
+
+    @GetMapping("/clear")
+    String clear(@RequestParam("name") String cacheName){
+        cacheService.clear(cacheName);
+        return "ok";
+    }
+
+
+
+
 
 }
